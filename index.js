12446468d03a2ddacf0669ba6eccb2abf73c7075
Melissa Brazle <melissaabrazle@outlook.com>
2023-05-25 02:59:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//remove main 
+const main = document.querySelector('main#main');
+main.remove();
+
+// create newHeader variable 
+const newHeader = document.createElement("h1");
+
+// create id = "victoy"
+ newHeader.id = "victory";
+
+// add text
+newHeader.innerHTML = "Melissa is the champion";
+
